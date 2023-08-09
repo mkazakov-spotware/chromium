7fe95449d0d073604a51c1e13689b283609422ea
Alex <a.rttr@yandex.ru>
2023-08-09 21:15:31 +0300

    bump up dotnet version in env.bat

--- algo/win/env.bat
+++ algo/win/env.bat
@@ -3,7 +3,7 @@
 rem SET __CT_DOTNET_PATH_x64=C:\Program Files\dotnet
 rem SET __CT_DOTNET_PATH_x86=C:\Program Files (x86)\dotnet
 SET __CT_DOTNET_PATH=C:\Program Files (x86)\dotnet
-SET __CT_HOSTFXR_PATH=C:\Program Files (x86)\dotnet\host\fxr\6.0.20\hostfxr.dll
+SET __CT_HOSTFXR_PATH=C:\Program Files (x86)\dotnet\host\fxr\6.0.21\hostfxr.dll
 SET __CT_PRODUCT_PATH=C:\Users\alex\lab\chromium2\src\out
 
 SET __CT_DOTNET_gcServer=1
